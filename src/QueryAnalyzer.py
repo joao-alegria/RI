8b7eb5b0645314ec7622bd52caf572347de81502
joaoalegria <joaops.alegria@gmail.com>
2019-12-09 15:37:05 +0000

    confirmed query analyzer calculations

--- src/QueryAnalyzer.py
+++ src/QueryAnalyzer.py
@@ -40,17 +40,13 @@ def calculateP_R_F1(truth, prediction):
         else:
             fp += 1
     for t in truth:
-        fn += 1
-
-    P = tp/(tp+fp) if (tp+fp) != 0 else 0.0
+        if t not in prediction:
+            fn += 1
+        P = tp/(tp+fp) if (tp+fp) != 0 else 0.0
     R = tp/(tp+fn) if (tp+fn) != 0 else 0.0
     F1 = (2*R*P)/(R+P) if (R+P) != 0 else 0.0
 
     return P, R, F1
-
-
-def calculateMAP(meanAPs):
-    return sum(meanAPs)/len(meanAPs)
 
 
 def calculateMPatK(truth, prediction, k):
@@ -69,11 +65,13 @@ def calculateNDCG(truth, prediction):
     actual = 0
     predictionRelevance = []
     for p in prediction:
+        wrote = False
         for t, tv in truth:
             if p == t:
+                wrote = True
                 predictionRelevance.append(tv)
-            else:
-                predictionRelevance.append(0)
+        if not wrote:
+            predictionRelevance.append(0)
 
     idealRelevance = [x[1] for x in truth]
     idealRelevance.sort(reverse=True)
@@ -87,23 +85,28 @@ def calculateNDCG(truth, prediction):
 
 
 def main(argv):
-    if len(argv) != 2:
+    if len(argv) != 3:
         print("""USAGE:
-        python3 QueryAnalyzer.py queryTruth queryResultFolder
+        python3 QueryAnalyzer.py queryTruth queryResultFolder outputFile
         """)
         sys.exit(0)
     queryTruth = argv[0]
     ourResultsFolder = argv[1]
+    outputFilename = argv[2]
 
     queryTrueResults = loadQueryTruth(queryTruth)
 
     ourResults = os.listdir(ourResultsFolder)
-    meanPs = []
+    mps = []
     precisions = []
     recalls = []
     f1s = []
     mpat10s = []
     ndcgs = []
+
+    outFile = open(outputFilename, "w")
+    outFile.write(
+        "Query, Precision, Recall, F1, MeanPrecision, MeanPrecision@10, NDCG\n")
     for f in ourResults:
         truth = queryTrueResults[f]
         trueDocs = [x[0] for x in truth]
@@ -111,17 +114,25 @@ def main(argv):
         P, R, F1 = calculateP_R_F1(trueDocs, results)
         mpat10 = calculateMPatK(trueDocs, results, 10)
         ndcg = calculateNDCG([[x[0], 3-int(x[1])] for x in truth], results)
-        meanPs.append(calculateMPatK(trueDocs, results, len(results)))
+        mp = calculateMPatK(trueDocs, results, len(results))
+        mps.append(mp)
         precisions.append(P)
         recalls.append(R)
         f1s.append(F1)
         mpat10s.append(mpat10)
         ndcgs.append(ndcg)
-        print(f, P, R, F1, mpat10, ndcg)
-    meanAP = calculateMAP(meanPs)
-    print(meanAP)
-    print(sum(precisions)/len(precisions))
-    print(sum(ndcgs)/len(ndcgs))
+        outFile.write("{},{},{},{},{},{},{}\n".format(
+            f, P, R, F1, mp, mpat10, ndcg))
+
+    aP = sum(precisions)/len(precisions)
+    aR = sum(recalls)/len(recalls)
+    aF1 = sum(f1s)/len(f1s)
+    aMP = sum(mps)/len(mps)
+    aMP10 = sum(mpat10s)/len(mpat10s)
+    aNDCG = sum(ndcgs)/len(ndcgs)
+
+    outFile.write("{},{},{},{},{},{},{}\n".format(
+        "Avg", aP, aR, aF1, aMP, aMP10, aNDCG))
 
 
 if __name__ == "__main__":
